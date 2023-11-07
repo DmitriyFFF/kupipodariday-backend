b9ac14e69449b542369641f9ce56acb5479c0454
Dmitriy Ferafontov <dmitriy.ferafontov@yandex.ru>
2023-11-07 23:37:37 +0400

    feat: add auth module

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,65 @@
+import {
+  Controller,
+  Req,
+  Post,
+  Body,
+  UseGuards,
+  // Get,
+  // Patch,
+  // Param,
+  // Delete,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+// import { CreateAuthDto } from './dto/create-auth.dto';
+// import { UpdateAuthDto } from './dto/update-auth.dto';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UsersService } from 'src/users/users.service';
+import { LocalGuard } from './guards/local.guard';
+import { JwtGuard } from './guards/jwt.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UsersService,
+  ) {}
+
+  @UseGuards(LocalGuard)
+  @Post('signin')
+  signin(@Req() req) {
+    return this.authService.auth(req.user);
+  }
+
+  @UseGuards(JwtGuard) //??
+  @Post('signup')
+  async signup(@Body() createUserDto: CreateUserDto) {
+    const user = await this.userService.create(createUserDto);
+
+    return this.authService.auth(user);
+  }
+
+  // @Post()
+  // create(@Body() createAuthDto: CreateAuthDto) {
+  //   return this.authService.create(createAuthDto);
+  // }
+
+  // @Get()
+  // findAll() {
+  //   return this.authService.findAll();
+  // }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.authService.findOne(+id);
+  // }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
+  //   return this.authService.update(+id, updateAuthDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.authService.remove(+id);
+  // }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+// import { CreateAuthDto } from './dto/create-auth.dto';
+// import { UpdateAuthDto } from './dto/update-auth.dto';
+import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt/dist';
+import { User } from 'src/users/entities/user.entity';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async auth(user: User) {
+    const payload = { sub: user.id };
+
+    return { access_token: this.jwtService.sign(payload) };
+  }
+
+  async validatePassword(username: string, password: string): Promise<any> {
+    const user = await this.usersService.findByUsername(username);
+
+    if (user && user.password === password) {
+      const { password, ...result } = user;
+
+      return result;
+    }
+    return null;
+  }
+
+  // create(createAuthDto: CreateAuthDto) {
+  //   return 'This action adds a new auth';
+  // }
+
+  // findAll() {
+  //   return `This action returns all auth`;
+  // }
+
+  // findOne(id: number) {
+  //   return `This action returns a #${id} auth`;
+  // }
+
+  // update(id: number, updateAuthDto: UpdateAuthDto) {
+  //   return `This action updates a #${id} auth`;
+  // }
+
+  // remove(id: number) {
+  //   return `This action removes a #${id} auth`;
+  // }
+}
